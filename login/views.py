afb7678c13989ac31b6a82cda4b063f1ebb198c4
wen <1010556575@qq.com>
2017-11-29 17:05:14 +0800

    first modify

---
+++ login/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from login import models
+from login import forms
+import hashlib
+
+# Create your views here.
+
+
+def index(request):
+    pass
+    return render(request, 'login/index.html')
+
+
+def login(request):
+    if request.session.get('is_login', None):
+        return redirect("/index/")
+    if request.method == "POST":
+        login_form = forms.UserForm(request.POST)
+        message = "请检查填写的内容！"
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    # if user.password == hash_code(password):
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.name
+                    return redirect('/index/')
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户不存在！"
+        return render(request, 'login/login.html', locals())
+
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
+
+def register(request):
+    pass
+    return render(request, 'login/register.html')
+
+
+def logout(request):
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect("/login/")
+    request.session.flush()
+    return redirect("/login/")
+
+def hash_code(s, salt='mysite'):
+    h = hashlib.sha256()
+    s += salt
+    h.update(s.encode())  # update方法只接收bytes类型
+    return h.hexdigest()
